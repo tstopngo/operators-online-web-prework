91f28bbba94ffc9165e3b7a337f9b447df386cd9
Tracy Ngo <tngo1206@gmail.com>
2019-02-04 00:35:29 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-if speed <= 40 || speed >=60
+if speed < 40 || speed >60
   return "Going 30 mph on the freeway would be unsafe, as would going 95 mph"
 else 
   return "That's within the \"safe\" range"
@@ -8,7 +8,7 @@ end
 
 
 def not_safe?(speed)
-	speed <= 40 || speed >=60 ? "Going 30 mph on the freeway would be unsafe, as would going 95 mph" : "That's within the \"safe\" range"
+	speed < 40 || speed >60 ? "Going 30 mph on the freeway would be unsafe, as would going 95 mph" : "That's within the \"safe\" range"
 end
 	
 
