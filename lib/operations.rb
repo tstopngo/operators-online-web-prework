fda4dd0248551b96e673b7d285145b9d99ebf60f
Tracy Ngo <tngo1206@gmail.com>
2019-02-04 00:37:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,7 +8,7 @@ end
 
 
 def not_safe?(speed)
-	speed < 40 || speed >60 ? "Going 30 mph on the freeway would be unsafe, as would going 95 mph" : "That's within the \"safe\" range"
+	speed < 40 || speed >60 ? true : false
 end
 	
 
